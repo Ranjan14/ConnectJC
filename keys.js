c2836e54af0258e9555e65abcf5e2c236037db94
Ranjan14 <63440245+Ranjan14@users.noreply.github.com>
2021-05-30 16:55:38 +0530

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,4 @@
+module.exports={
+    MONGOURI:"mongodb+srv://Ranjan_M_B:QBvQsWYnmdOXXJci@cluster0.btrhx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+        JWT_SECRET:"thfdteiklghtdkgjlfkl"
+}
